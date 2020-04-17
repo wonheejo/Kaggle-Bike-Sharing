7a3459cad5ed872794bdc65b96c6c935ea86a187
wonheejo <wonheejo@gmail.com>
2020-04-17 16:47:55 +0900

    Updated late

--- DataAnalysis2.py
+++ DataAnalysis2.py
@@ -3,7 +3,8 @@ import numpy as np
 import calendar
 from datetime import datetime
 from sklearn.model_selection import cross_val_score, KFold
-from sklearn.ensemble import RandomForestRegressor
+from sklearn.linear_model import LinearRegression
+from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor
 from sklearn.metrics import make_scorer
 
 pd.options.display.width = 0
@@ -61,12 +62,13 @@ train = train.drop(drop_features, axis=1)
 test = test.drop(drop_features, axis=1)
 
 # Rmsle scorer
-def rmsle(predict, actual):
-	predict = np.array(predict)
-	actual = np.array(actual)
+def rmsle(predict, actual, convertEXP=True):
+	if convertEXP:
+		predict = np.exp(predict),
+		actual = np.exp(actual)
 
-	plog = np.log(predict + 1)
-	alog = np.log(actual + 1)
+	plog = np.nan_to_num(np.array([np.log(v+1) for v in predict]))
+	alog = np.nan_to_num(np.array([np.log(v+1) for v in actual]))
 
 	dif = (plog-alog)**2
 	return np.sqrt(np.mean(dif))
@@ -77,18 +79,38 @@ rmsle_scorer = make_scorer(rmsle)
 k_fold = KFold(n_splits=10, shuffle=True, random_state=0)
 
 # Modeling
+
+# Linear Regression
+lr = LinearRegression()
+y_train_log = np.log1p(y_train)
+lr.fit(train, y_train_log)
+prediction = lr.predict(train)
+print("RMSLE Value For Linear Regression: ",rmsle(np.exp(y_train_log),np.exp(prediction),False))
+
 # Random Forest Regressor
-rf_reg = RandomForestRegressor(n_estimators=100, n_jobs=-1, random_state=0)
-rf_reg.fit(train, y_train)
-rf_reg_cvs = cross_val_score(rf_reg, train, y_train, cv=k_fold, scoring=rmsle_scorer).mean()
-print(rf_reg_cvs)
+rf_reg = RandomForestRegressor(n_estimators=500, n_jobs=-1, max_features='auto')
+rf_reg.fit(train, y_train_log)
+rf_reg_score = rf_reg.predict(train)
+print('RMSLE Value For Random Forest Regression: ', rmsle(np.exp(y_train_log), np.exp(rf_reg_score), False))
 
-"""
+# Gradient Boosting Regressor
+gbr_reg = GradientBoostingRegressor(n_estimators=4000, alpha=0.01)
+gbr_reg.fit(train, y_train_log)
+gbr_reg_score = gbr_reg.predict(train)
+print('RMSLE Value for Gradient Boosting Regressor: ', rmsle(np.exp(y_train_log), np.exp(gbr_reg_score), False))
+
+
 predictions = rf_reg.predict(test)
-
+"""
 submission = pd.read_csv('sampleSubmission.csv')
 submission['count'] = predictions
 
-submission.to_csv("Score_{0:.5f}_submission.csv".format(rf_reg_cvs), index=False)
+submission.to_csv("Score_{0:.5f}_submission.csv".format(rmsle(np.exp(y_train_log), np.exp(gbr_reg_score), False)), index=False)
 """
+submission = pd.DataFrame({
+        "datetime": datetimecol,
+        "count": [max(0, x) for x in np.exp(predictions)]
+    })
+submission.to_csv("Score_{0:.5f}_submission2.csv".format(rmsle(np.exp(y_train_log), np.exp(rf_reg_score), False)), index=False)
+
 
